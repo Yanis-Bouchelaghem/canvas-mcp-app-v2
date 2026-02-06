b8b2676544e6d5d86f22d7e8ae01f45850ce6541
Yanis <lastblast@outlook.fr>
2026-02-06 23:53:12 +0100

    Implement MCP sever with express

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
 import { createServer } from "./server.js";
 import cors from "cors";
 import express from "express";
@@ -5,6 +6,29 @@ import express from "express";
 const port = parseInt(process.env.PORT ?? "3001", 10);
 const app = express();
 app.use(cors());
+
+app.all("/mcp", async (request, result) => {
+    const mcpServer = createServer()
+    const transport = new StreamableHTTPServerTransport({
+        sessionIdGenerator: undefined // Stateless MCP
+    });
+    result.on("close", async () => {
+        try
+        {
+            await transport.close();
+            await mcpServer.close();
+        }
+        catch(error)
+        {
+            console.error(error);
+        }
+    });
+    
+    await mcpServer.connect(transport);
+    await transport.handleRequest(request, result, request.body);
+});
+
+
 
 app.listen(port, () => {
     console.log(`MCP server listening on http://localhost:${port}/mcp`);

366b00a8acd109ff19243bb9c81e4c6dfffaed1e
Yanis <lastblast@outlook.fr>
2026-02-07 13:44:29 +0100

    Implement session-based MCP endpoints.

--- main.ts
+++ main.ts
@@ -9,9 +9,10 @@ const port = parseInt(process.env.PORT ?? "3001", 10);
 const transports: Record<string, StreamableHTTPServerTransport> = {};
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.post("/mcp", async (request, result) => {
-    // Creates a new session, or resumes if session already exists.
+    // Creates a new session, or forwards the request to the existing session.
     const sessionId = request.headers["mcp-session-id"] as string | undefined;
     if (sessionId && transports[sessionId]) {
         await transports[sessionId].handleRequest(request, result, request.body);
@@ -21,7 +22,6 @@ app.post("/mcp", async (request, result) => {
     if (!isInitializeRequest(request.body)) {
         result.status(400).json({
             jsonrpc: "2.0",
-            // I don't like this, why can't we raise en MCP error lol
             error: { code: ErrorCode.InvalidRequest, message: "Bad Request: No valid session ID provided" },
             id: null,
         });
@@ -42,6 +42,26 @@ app.post("/mcp", async (request, result) => {
     const mcpServer = createServer();
     await mcpServer.connect(transport);
     await transport.handleRequest(request, result, request.body);
+});
+
+app.get("/mcp", async (request, result) => {
+    // Forwards the request to the existing session.
+    const sessionId = request.headers["mcp-session-id"] as string | undefined;
+    if (!sessionId || !transports[sessionId]) {
+        result.status(400).send("Invalid or missing session ID");
+        return;
+    }
+    await transports[sessionId].handleRequest(request, result);
+});
+
+app.delete("/mcp", async (request, result) => {
+    // Forwards the request to the existing session.
+    const sessionId = request.headers["mcp-session-id"] as string | undefined;
+    if (!sessionId || !transports[sessionId]) {
+        result.status(400).send("Invalid or missing session ID");
+        return;
+    }
+    await transports[sessionId].handleRequest(request, result);
 });
 
 

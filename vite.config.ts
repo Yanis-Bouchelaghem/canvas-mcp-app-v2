445775b7e7e069eb629036cee913fcc2aebf2bd0
Yanis <lastblast@outlook.fr>
2026-02-07 11:41:36 +0100

    Update configs for first run

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
     plugins: [react(), viteSingleFile()],
+    build: {
+        rollupOptions: {
+            input: process.env.INPUT,
+        },
+        outDir: "dist", // Output to dist directory
+        emptyOutDir: false, // Don't delete existing files in dist.
+    }
 })
